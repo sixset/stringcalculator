5304dc130cda7a3f7d4a5241d2dfdbc600b03067
SixSet <d.martyniak@dm-dev.pl>
2018-02-25 17:57:24 +0100

    Allow the Add method to handle an two of numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,7 +4,12 @@ public class StringCalculator {
     public int add(String numbers){
         if (numbers.isEmpty()){
             return 0;
+        }else if (numbers.contains(",")){
+            String [] numbersTab = numbers.split(",");
+            return Integer.valueOf(numbersTab[0])+Integer.valueOf(numbersTab[1]);
+        }else {
+            return Integer.valueOf(numbers);
         }
-        return -1;
+
     }
 }
--- src/test/java/TestStringCalculator.java
+++ src/test/java/TestStringCalculator.java
@@ -1,11 +1,29 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestStringCalculator {
 
+   private StringCalculator stringCalculator;
+
+    @Before public void onCreateCalc() {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test
     public void shouldReturnZeroOnEmptyString() {
-        StringCalculator stringCalculator = new StringCalculator();
         Assert.assertEquals(0, stringCalculator.add(""));
     }
+
+    @Test
+    public void inOneNumberShouldReturnSameNumber() {
+        Assert.assertEquals(3, stringCalculator.add("3"));
+    }
+
+    @Test
+    public void inTwoNumbersShouldReturnSum() {
+        Assert.assertEquals(8, stringCalculator.add("3,5"));
+    }
+
+
 }
